ef7ceb63a20a73327cccc815fe0d9c6e4f3769d7
matt-aa <matthew.j.alexander@outlook.com>
2025-11-24 19:33:55 +0000

    again

--- create_table.sql
+++ create_table.sql
@@ -4,17 +4,18 @@
 
 
 CREATE TABLE public.young_people_finances (
-id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-created_at timestamptz DEFAULT now(),
-age_range text NOT NULL,
-postcode text,
-salary numeric,
-savings numeric,
-debt numeric,
-mortgage numeric,
-rent numeric,
-property_value numeric
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at timestamptz DEFAULT now(),
+  age_range text NOT NULL,
+  postcode text,
+  salary numeric,
+  savings numeric,
+  debt numeric,
+  mortgage numeric,
+  rent numeric,
+  property_value numeric
 );
+
 
 
 -- Turn OFF RLS for testing (optional)
